e17fad742eb8261a3cf797bad6df610c6fb17e8d
unknown <joshalexjacobs@gmai.com>
2019-01-26 10:56:02 -0500

    finished text drag and drop

---
+++ Assets/Scripts/ItemDragHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler  {
+
+    public Canvas canvas;
+
+    public void OnDrag(PointerEventData eventData) {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        transform.position = Input.mousePosition;
+        transform.parent = canvas.transform;
+
+        ItemDropHandler[] dropHandlers = GameObject.FindObjectsOfType<ItemDropHandler>();
+        foreach(ItemDropHandler dropHandler in dropHandlers) {
+            RectTransform rectTransform = dropHandler.transform as RectTransform;
+            if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition)) {
+                transform.position = dropHandler.transform.position;
+                transform.parent = dropHandler.transform;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ItemDropHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDropHandler : MonoBehaviour, IDropHandler {
+
+    public void OnDrop(PointerEventData eventData) {
+        RectTransform sentenceLine = transform as RectTransform;
+
+        if(!RectTransformUtility.RectangleContainsScreenPoint(sentenceLine, Input.mousePosition)) {
+            
+        }
+    }
+}
