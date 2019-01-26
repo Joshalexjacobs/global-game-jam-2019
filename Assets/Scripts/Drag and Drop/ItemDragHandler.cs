db92dd2c53f334d07932fd86d2e32ebd5e9625c5
unknown <joshalexjacobs@gmai.com>
2019-01-26 13:25:59 -0500

    changed canvas to be camera space

--- Assets/Scripts/Drag and Drop/ItemDragHandler.cs
+++ Assets/Scripts/Drag and Drop/ItemDragHandler.cs
@@ -8,17 +8,22 @@ public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler  {
     public Canvas canvas;
 
     public void OnDrag(PointerEventData eventData) {
-        transform.position = Input.mousePosition;
+        Vector3 screenPoint = Input.mousePosition;
+        screenPoint.z = 100.0f;
+        transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
     }
 
     public void OnEndDrag(PointerEventData eventData) {
-        transform.position = Input.mousePosition;
+        Vector3 screenPoint = Input.mousePosition;
+        screenPoint.z = 100.0f;
+
+        transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
         transform.parent = canvas.transform;
 
         ItemDropHandler[] dropHandlers = GameObject.FindObjectsOfType<ItemDropHandler>();
         foreach(ItemDropHandler dropHandler in dropHandlers) {
             RectTransform rectTransform = dropHandler.transform as RectTransform;
-            if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition)) {
+            if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Camera.main.ScreenToWorldPoint(screenPoint))) {
                 transform.position = dropHandler.transform.position;
                 transform.parent = dropHandler.transform;
             }
